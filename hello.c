3f1786033b32af4cc82f5563183d1e8adaf1edcc
MaStR <105424373+youRmastr@users.noreply.github.com>
2022-05-12 15:36:56 +0545

    source code
    03 C tutorials| Fundamentals (Constants, variables, data types, formatted I/O functions, user input) code

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+Document Title: learning hello program
+author: Satya Raj Awasthi
+written on: 8/01/2078
+*/
+
+#include<stdio.h>
+
+void main() // this is a main function
+
+{
+
+printf("\n \t this is our first C program \n\n");
+
+}
